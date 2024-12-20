cc7f3844452db9743126bfcc9cc401c1d9c5ed77
guilherme1s <2000guilherme.silva@gmail.com>
2024-12-20 10:09:37 -0300

    feat: adjust about and card

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,7 +6,7 @@ export function About() {
             <S.AboutTitle>Sobre mim</S.AboutTitle>
 
             <S.AboutParagraph>
-                Formado em <span>Engenharia da Computação</span>, sou <span>Desenvolvedor Fullstack</span> com experiência em tecnologias 
+                Formado em <span>Engenharia da Computação</span>, sou <span>Desenvolvedor Front-End</span> com experiência em tecnologias 
                 como: <span>HTML, CSS, JavaScript, TypeScript, React e Next.js</span>. Minha paixão é criar interfaces 
                 eficientes e funcionais, focando em uma experiência de usuário excepcional.
             </S.AboutParagraph>
